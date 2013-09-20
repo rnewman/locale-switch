8ca51e9e8f945b7d5cd04cbca428cf9adc67d760
Richard Newman <rnewman@twinql.com>
2013-09-20 11:37:50 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+About Pages Add-on for Firefox on Android
+============================================
+
+This add-on adds menu items to open some of Firefox's special pages.
---
+++ bootstrap.js
@@ -0,0 +1,88 @@
+const { classes: Cc, interfaces: Ci, utils: Cu } = Components;
+
+Cu.import("resource://gre/modules/FileUtils.jsm");
+Cu.import("resource://gre/modules/Services.jsm");
+
+var gNativeWindow;
+var gCrashesMenuId;
+
+function LOG(msg) {
+  Services.console.logStringMessage("About Pages Add-On -- " + msg);
+}
+
+function aboutCrashes() {
+  window.open("about:crashes");
+}
+
+function loadIntoWindow(window) {
+  gNativeWindow = window.NativeWindow;
+
+  gCrashesMenuId = gNativeWindow.menu.add({
+    name: "about:crashes",
+    callback: aboutCrashes,
+    parent: gNativeWindow.menu.toolsMenuID
+  });
+}
+
+function unloadFromWindow(window) {
+  gNativeWindow.menu.remove(gCrashesMenuId);
+}
+
+/**
+ * bootstrap.js API
+ */
+var windowListener = {
+  onOpenWindow: function(aWindow) {
+    // Wait for the window to finish loading
+    let domWindow = aWindow.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIDOMWindowInternal || Ci.nsIDOMWindow);
+    domWindow.addEventListener("load", function() {
+      domWindow.removeEventListener("load", arguments.callee, false);
+      loadIntoWindow(domWindow);
+    }, false);
+  },
+  
+  onCloseWindow: function(aWindow) {
+  },
+  
+  onWindowTitleChange: function(aWindow, aTitle) {
+  }
+};
+
+function startup(aData, aReason) {
+  let wm = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator);
+
+  // Load into any existing windows
+  let windows = wm.getEnumerator("navigator:browser");
+  while (windows.hasMoreElements()) {
+    let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
+    loadIntoWindow(domWindow);
+  }
+
+  // Load into any new windows
+  wm.addListener(windowListener);
+}
+
+function shutdown(aData, aReason) {
+  // When the application is shutting down we normally don't have to clean
+  // up any UI changes made
+  if (aReason == APP_SHUTDOWN)
+    return;
+
+  let wm = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator);
+
+  // Stop listening for new windows
+  wm.removeListener(windowListener);
+
+  // Unload from any existing windows
+  let windows = wm.getEnumerator("navigator:browser");
+  while (windows.hasMoreElements()) {
+    let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
+    unloadFromWindow(domWindow);
+  }
+}
+
+function install(aData, aReason) {
+}
+
+function uninstall(aData, aReason) {
+}
